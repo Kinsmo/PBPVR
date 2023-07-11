74234f7a1ff6defb2968a98d7826d180dc3fd6fe
kinsmo <yxzhang92@163.com>
2023-07-11 16:36:27 +0200

    Update Demo 2 EDPVR and ESPVR.py

--- Demo 2 EDPVR and ESPVR.py
+++ Demo 2 EDPVR and ESPVR.py
@@ -1,8 +1,8 @@
 """
-Physics-based Pressure-Volume Relationship (PBPVR)
+Physics-based pressure-volume relationship (PBPVR) model for heart research.
 Date: 2023-02-22
-Author: Yunxiao Zhang
-Email: yunxiao9277@gmail.com
+Author: Yunxiao Zhang, Moritz Kalhöfer-Köchling, Eberhard Bodenschatz, and Yong Wang
+Email: yunxiao.zhang@ds.mpg.de; yong.wang@ds.mpg.de
 Cite it: 
 """
 
@@ -12,30 +12,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-"(01 Input parameters)"
+# Input parameters
 
-"1 sarcomere parameter"
+# Sarcomere parameter
 lamd_0=1.58/1.85
 
-"2 Normalized Thickness"
+# Normalized Thickness
 D = 0.27
 
-"3 Passive Property"
+# Passive Property
 a = 1 #[kPa]
 a = kpa_to_mmhg(a)
 b = 3.8
 
-"4 Contractity"
+# Contractity
 Ta = 75 #[kPa]
 Ta = kpa_to_mmhg(Ta)
 
-"5 Ratio of passive and active parts"
-"r_active = 0 yiels EDPVR"
-"r_passive = 1, r_active = 1 yiels ESPVR"
+# Ratio of passive and active parts
+# "r_active = 0 yiels EDPVR"
+# "r_passive = 1, r_active = 1 yiels ESPVR"
 
 
 
-"(02 Input Volumes)"
+# Input Volumes
 vn2_esv = np.arange(0.52,1.2,0.01)
 vn2_edv = np.arange(1.2,2.2,0.01)
 p= [PBPVR(v,D,a,b,Ta,1,0) for v in vn2_edv] #[mmHg]
@@ -45,7 +45,7 @@ p= [PBPVR(v,D,a,b,Ta,1,1) for v in vn2_esv] #[mmHg]
 plt.plot(vn2_esv,p,lw=4,color=colors[1],label=f"ESPVR")
 
 
-"(03 Plot)"
+# Plot
 plt.axvline(1,color='k')
 plt.axhline(0,color='k')
 plt.xlabel(r"Normalized Volume ($V/V_0$)")
